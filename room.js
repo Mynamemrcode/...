549363d3160748a7be170f982effcdadf958cfbe
Mynamemrcode <75869093+Mynamemrcode@users.noreply.github.com>
2021-05-07 16:32:56 -0700

    Add files via upload

--- room.js
+++ room.js
@@ -10,11 +10,31 @@ var firebaseConfig = {
     appId: "1:292568663878:web:35d1928b5b16fb6f394747"
   };
   // Initialize Firebase
+  var username = localStorage.getItem("Username"); 
+  document.getElementById("username").innerHTML = "Welcome! " + username;
+  function add() {
+
+    var addroom = document.getElementById("Inputhash").value;
+    firebase.database().ref("/").child(addroom).update({
+          yup : "Add That Room!"
+    
+    });
+    
+    localStorage.setItem("Addroom", addroom);
+    
+    window.location = "what.html";
+    
+    }
+  
+
+
+
   firebase.initializeApp(firebaseConfig);
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
      Room_names = childKey;
     //Start code
-
+  row= "<div class='roomname' id="+Room_names+" onclick='redirectToRoomName(this.id)'>#"+Room_names+"</div> <hr> ";
     //End code
     });});}
 getData();
+
